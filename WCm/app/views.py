727abb90946967ef872a6725af8d1cbce40fc4d1
Maaanzana <samanthajasminaguileraalfaro@gmail.com>
2024-10-18 14:41:29 -0300

    prueba

--- WCm/app/views.py
+++ WCm/app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def Hello(request, usuario):
+    return HttpResponse("Hola %s" % usuario)
+
+def About(request):
+    return render(request, 'about.html')
